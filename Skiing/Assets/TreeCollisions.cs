86218c09c3ebe41d264a6285eb7753d770947e7d
Migle Urbonaite <91160873+MistyDoe@users.noreply.github.com>
2023-02-16 07:42:36 +0100

    Added Tree Collision and more trees

--- Skiing/Assets/TreeCollisions.cs
+++ Skiing/Assets/TreeCollisions.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TreeCollisions : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public Color onHitColor;
+	public MeshRenderer mr;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void OnCollisionEnter(Collision collision)
+	{
+		mr.material.color = onHitColor;
+
+	}
 }
